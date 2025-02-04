dca6f7a51a2ec393851b0f91153fb65fc205f67c
designangl07 <designangel07@gmail.com>
2025-02-04 11:32:44 +0530

    config update for fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      domains: ['140.238.229.20'], // Ensure domain is correct
+    },
+    webpack(config) {
+        return config;
+      },
+  };
+  
+  export default nextConfig;
+  
