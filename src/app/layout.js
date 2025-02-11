b1847b5b75cd78ee23b934c328e4617f5712b0bf
designangl07 <designangel07@gmail.com>
2025-02-11 23:40:10 +0530

    added code for set up a Google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css'; // Import global CSS
 import ClientLayout from './ClientLayout';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
@@ -10,6 +11,21 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://use.typekit.net/vjg6msz.css" />
+
+        {/* Google Analytics Script */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KYGCJPMCM0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KYGCJPMCM0');
+          `}
+        </Script>
       </head>
       <body>
         {/* Client Component for Header, Footer, and Popup */}
